f6a76ebbeb4e951996bc7b691b3777c2adabae22
MOHAMMAD HASSNAIN <91183477+hassni12@users.noreply.github.com>
2022-10-25 17:18:45 +0500

    a

--- backend/config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-export const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3001',
-    'http://localhost:3000'
-];
-
-// module.exports = allowedOrigins;
